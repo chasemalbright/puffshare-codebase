ed0e81d3d48ce43afebadc7c408f495d29a6cb86
chase <chasemalbright33@gmail.com>
2021-08-23 12:28:32 -0500

    product and cart reduc and api calls

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,7 +2,13 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
+import { cartReducer } from './reducers/cartReducers'
+import { getProductsReducer, getProductDetailsReducer } from './reducers/productReducers'
+
 const reducer = combineReducers({
+    cart: cartReducer,
+    getProducts: getProductsReducer,
+    getDetails: getProductDetailsReducer
 
 })
 
