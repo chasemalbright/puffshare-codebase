2081c598803252d40505509059289299cc911b35
chase <chasemalbright33@gmail.com>
2022-05-23 11:52:02 -0500

    added features

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,17 +5,14 @@ const { getAllProducts, getProductById, updateProductCountById } = require('../c
 
 // get all products from db
 // GET /api/products
-
 router.get('/', getAllProducts);
 
 // get a products by id from db
 // GET /api/products/id
-
 router.get('/:id', getProductById);
 
 // update product inventory by id
 // PUT /api/products
-
 router.put('/update', updateProductCountById)
 
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ function App() {
   return (
     <Router>
         <Navbar click={() => setSideToggle(true)} />
+        
         <SideDrawer show={sideToggle} click={() => setSideToggle(false)} />
         <Backdrop show={sideToggle} click={() => setSideToggle(false)} />
 
